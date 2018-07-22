06b7afe5ea2890f0ba62ae8eb02d9cb457c82516
charleskoo <charles.kou@yamibuy.com>
2018-07-22 13:58:59 +0800

    init vue todolist project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <ul>
+      <li v-for="item in items" :class="{finished: item.isFinished}">
+        {{item.label}}
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'this is todo list app',
+      items: [{
+        label: 'coding',
+        isFinished: false
+      }, {
+        label: 'walking',
+        isFinished: true
+      }]
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.finished {
+  text-decoration: underline;
+}
+</style>

ab971e39d61bb520c4146d3e3a53dc78cc2ebb79
charleskoo <charles.kou@yamibuy.com>
2018-07-22 14:58:39 +0800

    localStorage存储

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
+    <input v-model="newItem" v-on:keyup.enter="addNew">
     <ul>
-      <li v-for="item in items" :class="{finished: item.isFinished}">
-        {{item.label}}
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem.label" v-bind:class="{finished: todoItem.isFinished}"
+        v-on:click="toggleFinish(todoItem)">
+        {{todoItem.label}}
       </li>
     </ul>
   </div>
@@ -12,20 +13,37 @@
 </template>
 
 <script>
+
+import Store from './store'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'this is todo list app',
-      items: [{
-        label: 'coding',
-        isFinished: false
-      }, {
-        label: 'walking',
-        isFinished: true
-      }]
+      msg: 'Todo List - 待办事项',
+      todoItems: Store.fetch(),
+      newItem: ''
     }
-
+  },
+  watch: {
+    todoItems: {
+      handler: function (items) {
+        Store.save(items);
+      },
+      deep: true
+    }
+  },
+  methods: {
+    toggleFinish: function (item) {
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function () {
+      this.todoItems.push({
+        label: this.newItem,
+        isFinished: false
+      });
+      this.newItem = '';
+    }
   }
 }
 </script>
@@ -36,12 +54,11 @@ h1, h2 {
   font-weight: normal;
 }
 ul {
-  list-style-type: none;
+  list-style: none;
   padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  height: 30px;
 }
 a {
   color: #42b983;
